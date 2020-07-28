50e1230c564426e8ea28d7b04eb8cfd19080a068
ThreeSnakes <dev.select995@gmail.com>
2020-07-29 01:27:55 +0900

    ADD: TitleSection Css
    - TitleSection에 Class 및 css 추가.

--- src/Components/TitleSection.js
+++ src/Components/TitleSection.js
@@ -4,6 +4,7 @@ const SUB_TITLE = "본인 명의의 핸드폰 번호를 입력하세요."
 export default class TitleSection {
   constructor({ $target }) {
     this.header = document.createElement('header');
+    this.header.className = "header";
 
     $target.appendChild(this.header);
     this.render();
@@ -11,9 +12,11 @@ export default class TitleSection {
 
   render() {
     const titleElem = document.createElement("h2");
+    titleElem.className = "main-title";
     titleElem.innerHTML = TITLE;
-    
+
     const spanElem = document.createElement("span");
+    spanElem.className = "sub-title";
     spanElem.innerHTML = SUB_TITLE;
 
     this.header.append(titleElem, spanElem);
--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,4 @@
+require('./css/style.css');
 import App from './App.js';
 
 const app = new App(document.querySelector('.app'));
